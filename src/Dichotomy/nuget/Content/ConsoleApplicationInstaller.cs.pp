53d4a2b5969495b28d39127c611d2d825b799c0d
John Nelson <jnelson@johncoder.com>
2012-07-09 13:09:37 -0400

    Added nuspec.

---
+++ src/Dichotomy/nuget/Content/ConsoleApplicationInstaller.cs.pp
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel;
+using Dichotomy.Helpers;
+
+namespace $rootnamespace$
+{
+    [RunInstaller(true)]
+    public class ConsoleApplicationInstaller : CustomInstaller
+    {
+        
+    }
+}
